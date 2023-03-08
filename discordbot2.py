e08747ca63d6bc6e910dc3f59d34f0e08535b31c
Adib akkari <75389300+adssib@users.noreply.github.com>
2023-03-08 15:08:54 -0500

    Add files via upload

---
+++ discordbot2.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+description = '''An example bot to showcase the discord.ext.commands extension module.There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+@bot.command()
+async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+            await guild.system_channel.send(to_send)
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+client.run('ODQ3MDUxOTIxMzM5NzExNDk4.YK4ckw.17dkDCEg2jnY4lCVN2MRrBD2mNA')
